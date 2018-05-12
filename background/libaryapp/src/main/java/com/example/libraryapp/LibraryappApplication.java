4afa43e55ef2a1ed453647a14c7be0d28a500740
Lee <1300335198@qq.com>
2018-05-12 16:06:08 +0800

    test

---
+++ background/libaryapp/src/main/java/com/example/libraryapp/LibraryappApplication.java
@@ -0,0 +1,12 @@
+package com.example.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryappApplication.class, args);
+    }
+}
