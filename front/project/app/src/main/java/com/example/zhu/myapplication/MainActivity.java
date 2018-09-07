550c0f6ef553dd5d5e198b0f168b9b2e77e82e03
AnswerChu <346589455@qq.com>
2018-09-07 11:23:49 +0800

    连接服务器尝试完成登录

---
+++ front/project/app/src/main/java/com/example/zhu/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.zhu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.send_request){
+            sendRequestWithOkHttp();
+        }
+    }
+    private void sendRequestWithOkHttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://39.105.105.105/costomer/admin/login.json")
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    parseJSONWithJSONObject(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    private void parseJSONWithJSONObject(String jsonData) {
+        try {
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String admin_account = jsonObject.getString("admin_account");
+                String admin_password = jsonObject.getString("admin_password");
+                Log.d("MainActivity", "admin_account is" +admin_account);
+                Log.d("MainActivity", "admin_password is" +admin_password);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
